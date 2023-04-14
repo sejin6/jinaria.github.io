8482b1e592bae8c000db8ee6b3d6ba4727a62a8c
sejin6 <wlstmdrl0925@gmail.com>
2023-04-15 03:15:06 +0900

    apply github vue

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: './index'
+  outputDir: './index',
+  publicPath: './index'
 })
